a0be94959a3f29fc59254bed9f0ac9678f98d5de
libin-n-g <libinngeorge@gmail.com>
2017-10-31 21:12:11 +0530

    adding posts to routes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\posts;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\PostsRequest;
 
 class PostsController extends Controller
 {
@@ -12,5 +14,20 @@ class PostsController extends Controller
     {
         $posts = posts::orderBy('posted_at', 'desc')->paginate(20);
         return view('home')->withposts($posts);
+    }
+    public function create(Request $request)
+    {
+      return view('posts.create');
+    }
+    public function store(PostsRequest $request)
+    {
+      $user = Auth::user();
+  
+      $post = $user->posts()->create([
+          'title' => $request->input('title'),
+          'content' => $request->input('content')
+      ]);
+  
+      return redirect()->route('posts.show', $post)->with('success', trans('posts.created'));
     }
 }
