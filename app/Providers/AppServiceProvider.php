b1dff7ed957e3661e506200e9cf5724b7bcef43f
libin-n-g <libinngeorge@gmail.com>
2018-02-05 16:49:59 +0530

    Change for working with mariadb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\posts;
 use Carbon\Carbon;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+    	Schema::defaultStringLength(191);
       	posts::creating(function ($post) {
       		$post->posted_at = Carbon::now();
         });
